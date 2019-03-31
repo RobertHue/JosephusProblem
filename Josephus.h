19a5b40ddb4151898015bf6c79cd0700160ad99c
RobertHue <robert.huem@gmail.com>
2019-03-31 21:33:21 +0200

    josephus problem solved v1.0

--- Josephus.h
+++ Josephus.h
@@ -2,13 +2,14 @@
 #define JOSEPHUS_H_
 
 #include <vector>
+#include <list>
 #include <iostream>
 
 template<typename T>
 void eraseVectorAtIndex(std::vector<T>& vec, int idx) {
-	std::vector<T>::const_iterator iter = vec.cbegin();
-	for (int j = 0; j < idx; ++j)	++iter;
-	vec.erase(iter);
+	std::vector<T>::const_iterator citer = vec.cbegin();
+	for (int j = 0; j < idx; ++j)	++citer;
+	vec.erase(citer);
 }
 
 
@@ -17,33 +18,54 @@ void eraseVectorAtIndex(std::vector<T>& vec, int idx) {
 /// @param1		K	the people being removed on the table
 /// @param2		evadedList	the ordered list of evaded people
 /// @ret		the remainder of the josephus problem
-int Josephus(int N, int K, std::vector<int>& evadedList) {
+int Josephus(int N, int K, std::vector<int>& evadedVectorList) {
 	// @TODO what happens if K is greater than N?
 
 	// setup the josephus problem for N
-	std::vector<int> list;
-	for (int i = 0; i < N; ++i) {
+	std::list<int> list;
+	for (int i = 1; i <= N; ++i) {
 		list.push_back(i);
 	}
 
+
+	std::list<int>::iterator iter = list.begin();
+	
+
 	// try to solve it
-	int evadeIndex = K - 1;
+	int evadeIndex = K -1;
 	while (list.size() > 1) {
+
+		for (int j = 0; j < K-1; ++j) {
+			++iter;
+
+			// if end of list is reached reset it (iterator out of bounds)
+			if (iter == list.end()) {
+				iter = list.begin();
+			}
+		}
+
 		// debug output: not really needed..
-		std::cout	<<	"removed at:"			<< evadeIndex 
-					<<	" - with value of: "	<< list.at(evadeIndex) 
-					<< std::endl;
+		std::cout << "removed value of: " << *iter << std::endl;
 
 		// add the evaded index to the evadedList parameter for testing 
 		// and erase it from josephus list
-		evadedList.push_back(list.at(evadeIndex));
-		eraseVectorAtIndex(list, evadeIndex);
+		evadedVectorList.push_back(*iter);
+
+		//std::list<int>::iterator tmpIter;
+		//if (iter == list.end() || iter == list.end()-1) { tmpIter = list.begin(); }
+		//else { tmpIter = iter + 1; }
+		iter = list.erase(iter); // iterator is invalid after erase, so TODO here...
+		//iter = tmpIter;
+		if (iter == list.end()) { iter = list.begin(); }
+		
+		 
+		//eraseVectorAtIndex(list, evadeIndex);
 		
 		// advance the index for the next iteration
-		evadeIndex += K-1;
-		if (evadeIndex > list.size()) {
-			evadeIndex = (evadeIndex % list.size())+1;
-		}
+		//evadeIndex += K-1;
+		//if (evadeIndex >= list.size()) {
+		//	evadeIndex = (evadeIndex % list.size());
+		//}
 	}
 
 	// return the remainder of the josephus problem
@@ -51,26 +73,35 @@ int Josephus(int N, int K, std::vector<int>& evadedList) {
 	return remainder;
 }
 
-/// leider hat es nicht so geklappt mit der Bewerbung
 #endif /* JOSEPHUS_H_ */
 
 
-
-
-
-
-
 //// :::FRAGENKATALOG:::
-// was bedeutet virtual?
-// was ist der unterschied zwischen einer klasse und einem objekt
-// wie kann ich informationen verstecken?
-// was ist der unterschied zwischen python und c++
-// gibt es einen unterschied bei tmeplates in java zu c++
-// wozu dienen templates?
+// Erzählen sie mal was über sich (10 min) - 
+//		hier am besten keine aufzählung; sondern eher die persönliche motivation
 
-
-// Erzählen sie mal was über sich (10 min) - keine aufzählung mehr sondern eher die motivation...
 // Wie schätzen sie sich ein in c++
-// die lebenshaltungskosten sind in münchen höher. mit 18€h hätte man kein informatik studieren brauchen...
+//		Relativ gut was die Grundlagen betrifft. oder bessser: solide Grundlagen?
+//		Was bibliotheken der STL/STD betrifft so muss ich noch manchmal nachschauen wie das war mit Methodenaufruf xy
 
-// MAS mixed mode ferchau vllt noch? ka ach was solls...
+// was ist der unterschied zwischen einer klasse und einem objekt
+// klasse ist bauplan eines objekts und objekt ist die instanz bzw. ausführung einer klasse, die dann auch Hauptspeicher belegt
+
+// wie kann ich informationen verstecken? 
+//		public private protected
+
+// was bedeutet virtual? 
+//		virtual ermöglicht es in C++ Objekte dynamisch zu binden; d.h. die Objekte bekommen von ihren Oberklassen virtual definierte Methoden mit 
+
+// was ist der unterschied zwischen python und c++
+//		in python wird der code zeilenweise interpretiert
+
+// würden Sie in Betracht ziehen python zu programmieren? würde das Ihnen spaß machen?
+//		ja aufjedenfall. In blender hab ich da auch mal was mit python probiert (menger_sponge) und während meiner tätigkeit bei der Bachelorarbeit konnte ich auch bereits einiges über python lernen. 
+//		Von dem her denke ich, dass mir die weitere Einarbeitung in python gut umsetzbar ist.
+
+// wozu dienen templates?
+//		um einen algorithmus/methode für unterschiedliche arten von typen (uint8, uint16) funktionsfähig zu machen.
+
+// gibt es einen unterschied bei templates in java zu c++
+//		habe das in java noch nie so wirklich im detail untersucht
--- tests/JosephusTest.cc
+++ tests/JosephusTest.cc
@@ -1,13 +1,15 @@
 #define BOOST_TEST_MODULE "JosephusModule"
 #include <boost/test/unit_test.hpp>
-#include <iostream>
-#include <string>
+#include <boost/test/unit_test_parameters.hpp>
 
 #include "Josephus.h"
 #include <list>
+#include <vector>
+
 
 BOOST_AUTO_TEST_CASE(ERASE_VECTOR_AT_INDEX)
 {
+	boost::unit_test::unit_test_log.set_threshold_level(boost::unit_test::log_all_errors);
 	BOOST_TEST_MESSAGE("Test the erasing vector at index of");
 
 	std::vector<int> vec1{ 1,2,3,4,5,6 };
@@ -23,19 +25,106 @@ BOOST_AUTO_TEST_CASE(ERASE_VECTOR_AT_INDEX)
 
 BOOST_AUTO_TEST_CASE( JOSEPHUS_TEST_N5K2 )
 {
-	BOOST_TEST_MESSAGE("Test the Josephus Algorithm with N=5 and K=2");
-
+	boost::unit_test::unit_test_log.set_threshold_level(boost::unit_test::log_all_errors);
 	int N = 5;
 	int K = 2;
-	std::vector<int> evadedList;
-	int remainder = Josephus(5, 2, evadedList);
-	/// 0 1 2 3 4 => 0 2 3 4 =>  0 2 4 => 2 4 => 4...
-	// sol : 1 3 0 4 , rem=2
+	BOOST_TEST_MESSAGE("Test the Josephus Algorithm with N=" << N << " and K= " << K << " !!!");
 
-	BOOST_CHECK(evadedList.at(0) == 1);
-	BOOST_CHECK(evadedList.at(1) == 3);
-	BOOST_CHECK(evadedList.at(2) == 0);
-	BOOST_CHECK(evadedList.at(3) == 4);
+	std::vector<int> evadedVectorList;
+	int remainder = Josephus(N, K, evadedVectorList);
+	/// 1 2 3 4 5 => 1 3 4 5 => 1 3 5 (1. durchlauf vorbei) => 3 5 => 3
+	// sol : 2 4 1 5 , rem=3
+	// math.sol: remainder = 1001 => 00011 = 3
 
-	BOOST_CHECK(remainder == 2);
+	BOOST_CHECK(evadedVectorList.at(0) == 2);
+	BOOST_CHECK(evadedVectorList.at(1) == 4);
+	BOOST_CHECK(evadedVectorList.at(2) == 1);
+	BOOST_CHECK(evadedVectorList.at(3) == 5);
+
+	BOOST_CHECK(remainder == 3);
 } 
+
+
+
+BOOST_AUTO_TEST_CASE(JOSEPHUS_TEST_N5K11)
+{
+	boost::unit_test::unit_test_log.set_threshold_level(boost::unit_test::log_all_errors);
+	int N = 5;
+	int K = 11;
+	BOOST_TEST_MESSAGE("Test the Josephus Algorithm with N=" << N << " and K= "<< K <<" !!!");
+
+	std::vector<int> evadedVectorList;
+	int remainder = Josephus(N, K, evadedVectorList);
+	// sol : 1  4 2 3  rem 5
+	// math.sol: remainder = 5^1 + 5^0 = 5 rem = 5
+
+	BOOST_CHECK(evadedVectorList.at(0) == 1);
+	BOOST_CHECK(evadedVectorList.at(1) == 4);
+	BOOST_CHECK(evadedVectorList.at(2) == 2);
+	BOOST_CHECK(evadedVectorList.at(3) == 3);
+
+	BOOST_CHECK(remainder == 5);
+}
+
+
+BOOST_AUTO_TEST_CASE(JOSEPHUS_TEST_N13K2)
+{
+	boost::unit_test::unit_test_log.set_threshold_level(boost::unit_test::log_all_errors);
+	int N = 13;
+	int K = 2;
+	BOOST_TEST_MESSAGE("Test the Josephus Algorithm with N=" << N << " and K= " << K << " !!!");
+
+	std::vector<int> evadedVectorList;
+	int remainder = Josephus(N, K, evadedVectorList);
+	/// 11
+	// sol : 2 4 6 8 10 12 (all even positions done) 1 5 9 13 7 3 rem 11
+	// math.sol: remainder = 1101 => 1011 = 11 
+
+	BOOST_CHECK(evadedVectorList.at(0) == 2);
+	BOOST_CHECK(evadedVectorList.at(1) == 4);
+	BOOST_CHECK(evadedVectorList.at(2) == 6);
+	BOOST_CHECK(evadedVectorList.at(3) == 8);
+	BOOST_CHECK(evadedVectorList.at(4) == 10);
+	BOOST_CHECK(evadedVectorList.at(5) == 12);
+	BOOST_CHECK(evadedVectorList.at(6) == 1);
+	BOOST_CHECK(evadedVectorList.at(7) == 5);
+	BOOST_CHECK(evadedVectorList.at(8) == 9);
+	BOOST_CHECK(evadedVectorList.at(9) == 13);
+	BOOST_CHECK(evadedVectorList.at(10) == 7);
+	BOOST_CHECK(evadedVectorList.at(11) == 3);
+
+	BOOST_CHECK(remainder == 11);
+}
+
+
+
+BOOST_AUTO_TEST_CASE(JOSEPHUS_TEST_N13K3)
+{
+	boost::unit_test::unit_test_log.set_threshold_level(boost::unit_test::log_all_errors);
+	int N = 13;
+	int K = 3;
+	BOOST_TEST_MESSAGE("Test the Josephus Algorithm with N=" << N << " and K= " << K << " !!!");
+
+	std::vector<int> evadedVectorList;
+	int remainder = Josephus(N, K, evadedVectorList);
+	///  
+	// sol: 3 6 9 12 2  7  11  4 10 5  1  8 rem 13
+
+	// math.sol: remainder = 3^2 + 3^1 + 3^0 = 13 so rem is 13
+
+
+	BOOST_CHECK(evadedVectorList.at(0) == 3);
+	BOOST_CHECK(evadedVectorList.at(1) == 6);
+	BOOST_CHECK(evadedVectorList.at(2) == 9);
+	BOOST_CHECK(evadedVectorList.at(3) == 12);
+	BOOST_CHECK(evadedVectorList.at(4) == 2);
+	BOOST_CHECK(evadedVectorList.at(5) == 7);
+	BOOST_CHECK(evadedVectorList.at(6) == 11);
+	BOOST_CHECK(evadedVectorList.at(7) == 4);
+	BOOST_CHECK(evadedVectorList.at(8) == 10);
+	BOOST_CHECK(evadedVectorList.at(9) == 5);
+	BOOST_CHECK(evadedVectorList.at(10) == 1);
+	BOOST_CHECK(evadedVectorList.at(11) == 8);
+
+	BOOST_CHECK(remainder == 13);
+}
