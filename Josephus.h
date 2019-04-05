f4dcf95f21fb11c460832b2eab94da86d6048959
RobertHue <robert.huem@gmail.com>
2019-04-05 02:28:23 +0200

    refactor: removed and added some comments to be more descriptive on what is done

--- Josephus.h
+++ Josephus.h
@@ -5,6 +5,9 @@
 #include <list>
 #include <iostream>
 
+/// @brief erases the element at given index idx inside the passed vector
+/// @param0		vec	the vector you want the index to be erased from
+/// @param1		idx	the index to be removed
 template<typename T>
 void eraseVectorAtIndex(std::vector<T>& vec, int idx) {
 	std::vector<T>::const_iterator citer = vec.cbegin();
@@ -20,18 +23,15 @@ void eraseVectorAtIndex(std::vector<T>& vec, int idx) {
 /// @ret		the remainder of the josephus problem
 int Josephus(int N, int K, std::vector<int>& evadedVectorList) {
 
-	// setup the josephus problem for N
+	// setup the josephus problem for N people (ID = i)
 	std::list<int> list;
 	for (int i = 1; i <= N; ++i) {
 		list.push_back(i);
 	}
-
-
 	std::list<int>::iterator iter = list.begin();
 	
-
 	// try to solve it
-	int evadeIndex = K -1;
+	// int evadeIndex = K -1;
 	while (list.size() > 1) {
 
 		for (int j = 0; j < K-1; ++j) {
@@ -43,28 +43,24 @@ int Josephus(int N, int K, std::vector<int>& evadedVectorList) {
 			}
 		}
 
-		// debug output: not really needed..
-		std::cout << "removed value of: " << *iter << std::endl;
-
-		// add the evaded index to the evadedList parameter for testing 
-		// and erase it from josephus list
+		// add the evaded index to the evadedList (param2)
 		evadedVectorList.push_back(*iter);
 
-		//std::list<int>::iterator tmpIter;
-		//if (iter == list.end() || iter == list.end()-1) { tmpIter = list.begin(); }
-		//else { tmpIter = iter + 1; }
-		iter = list.erase(iter); // iterator is invalid after erase, so TODO here...
-		//iter = tmpIter;
+		// erase element at iterator position and advance
+		iter = list.erase(iter);
+
+		// if iterator points to the end of the list then advance to the beginning again (ring buffer)
 		if (iter == list.end()) { iter = list.begin(); }
-		
-		 
-		//eraseVectorAtIndex(list, evadeIndex);
-		
+
+		// ::::::::::::::::::::::::::::::::::::::::::::::::::::
+		// :::::: different solution than with iterators ::::::
+		// ::::::::::::::::::::::::::::::::::::::::::::::::::::
+		// eraseVectorAtIndex(list, evadeIndex);
 		// advance the index for the next iteration
-		//evadeIndex += K-1;
-		//if (evadeIndex >= list.size()) {
-		//	evadeIndex = (evadeIndex % list.size());
-		//}
+		// evadeIndex += K-1;
+		// if (evadeIndex >= list.size()) {
+		//	 evadeIndex = (evadeIndex % list.size());
+		// }
 	}
 
 	// return the remainder of the josephus problem
@@ -73,34 +69,3 @@ int Josephus(int N, int K, std::vector<int>& evadedVectorList) {
 }
 
 #endif /* JOSEPHUS_H_ */
-
-
-//// :::FRAGENKATALOG:::
-// Erzählen sie mal was über sich (10 min) - 
-//		hier am besten keine aufzählung; sondern eher die persönliche motivation
-
-// Wie schätzen sie sich ein in c++
-//		Relativ gut was die Grundlagen betrifft. oder bessser: solide Grundlagen?
-//		Was bibliotheken der STL/STD betrifft so muss ich noch manchmal nachschauen wie das war mit Methodenaufruf xy
-
-// was ist der unterschied zwischen einer klasse und einem objekt
-// klasse ist bauplan eines objekts und objekt ist die instanz bzw. ausführung einer klasse, die dann auch Hauptspeicher belegt
-
-// wie kann ich informationen verstecken? 
-//		public private protected
-
-// was bedeutet virtual? 
-//		virtual ermöglicht es in C++ Objekte dynamisch zu binden; d.h. die Objekte bekommen von ihren Oberklassen virtual definierte Methoden mit 
-
-// was ist der unterschied zwischen python und c++
-//		in python wird der code zeilenweise interpretiert
-
-// würden Sie in Betracht ziehen python zu programmieren? würde das Ihnen spaß machen?
-//		ja aufjedenfall. In blender hab ich da auch mal was mit python probiert (menger_sponge) und während meiner tätigkeit bei der Bachelorarbeit konnte ich auch bereits einiges über python lernen. 
-//		Von dem her denke ich, dass mir die weitere Einarbeitung in python gut umsetzbar ist.
-
-// wozu dienen templates?
-//		um einen algorithmus/methode für unterschiedliche arten von typen (uint8, uint16) funktionsfähig zu machen.
-
-// gibt es einen unterschied bei templates in java zu c++
-//		habe das in java noch nie so wirklich im detail untersucht
